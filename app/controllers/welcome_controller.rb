7b5b1f385bad2029a243c310efee901fdfe423d0
ichy <ichylinux@gmail.com>
2013-08-16 11:02:37 +0900

    カレンダーの枠組み

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,15 @@
 class WelcomeController < ApplicationController
   
   def index
+    year = params[:year]
+    month = params[:month]
+    unless year.present? and month.present?
+      today = Date.today
+      year ||= today.year
+      month ||= today.month
+    end
     
+    @display_date = Date.new(year.to_i, month.to_i, 1)
   end
 
 end
--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,9 @@
+# coding: UTF-8
+
 module WelcomeHelper
+  
+  def day_of_week(index)
+    @names ||= %w{ 日 月 火 水 木 金 土 }
+    @names[index]
+  end
 end
