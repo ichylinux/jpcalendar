a1992b071c6ce18636b4325d6aee4f689fb46084
ichy <ichylinux@gmail.com>
2013-08-16 10:06:42 +0900

    初回コミット

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jpcalendar::Application.config.secret_token = '22a8ef72232cd9902cef21703138e438d47bfe0b3d40eab83cdd72d41f1ebe34a686d133bc0192cc85bcfc097645dc92bfaaa6ab37f148a5fada70257942fb7b'
